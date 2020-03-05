802b69935f03ceee107a7c2b10b205fd848e319c
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-05 14:18:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
-  if phrase.upcase
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.downcase
+  if phrase
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase.upcase
+  elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else phrase
+    return
   end
 end
 
