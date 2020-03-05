501726cc1d5d878ecd51620a088c810772ca8df3
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-05 14:26:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.downcase
+  elsif phrase = "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase = "Hi!"
+    return "HUH?! SPEAK UP, SONNY"
   end
 end
